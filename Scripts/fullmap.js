4687e7cba4395d3f763c7cf747799d4fc401e518
Ching CHEN <chingchen@gamania.com>
2022-06-27 16:26:39 +0800

    1213

--- Scripts/fullmap.js
+++ Scripts/fullmap.js
@@ -1,9 +1,12 @@
 function _initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
 		center: { lat: 25.0623841, lng: 121.1425703 },
-		zoom: 12
+		zoom: 12,
+		disableDefaultUI: true
 	});
 }
+let map = null;
+
 // 隱藏滾動軸
 if (!isMobile.any) {
 	$(".control-content").mCustomScrollbar();
@@ -105,3 +108,102 @@ $(".search-btn").on("click", function () {
 	});
 	console.log(obj);
 });
+
+// 地圖切換
+$(".map-btn__type").on("click", function () {
+	var type = $(this).attr("data-type");
+	if (type == "basic") {
+		map.setMapTypeId("roadmap");
+	}
+	if (type == "satellite") {
+		map.setMapTypeId("hybrid");
+	}
+});
+
+// 選單開啟
+$(".menu-open").on("click", function () {
+	$(".menu").addClass("show");
+});
+// 選單關閉
+$(".menu-close").on("click", function () {
+	$(".menu").removeClass("show");
+});
+
+// popup 取消
+$(".popup-btn__cancel").on("click", function () {
+	$(".popup-input__control").val("");
+	$(".popup-select").val(-1);
+	$(".popup-checkbox__control").each(function (i, v) {
+		$(v).prop("checked", false);
+	});
+	$(".popup").removeClass("show");
+});
+// popup 關閉
+$(".popup-close").on("click", function () {
+	$(".popup-input__control").val("");
+	$(".popup-select").val(-1);
+	$(".popup-checkbox__control").each(function (i, v) {
+		$(v).prop("checked", false);
+	});
+	$(".popup").removeClass("show");
+});
+// popup 關閉
+$(document).on("mouseup", function (e) {
+	var container = $(".popup-wrap");
+	if (!container.is(e.target) && container.has(e.target).length === 0) {
+		$(".popup").removeClass("show");
+	}
+});
+
+// 切換登入/登出文字狀態
+function toggleLoginText() {
+	// 登入 / 登出
+	if ($(".login-status").attr("data-status") == "login") {
+		$(".login-status").attr("data-status", "logout");
+		$(".login-status").find(".menu-li__text").text("登出");
+	} else {
+		$(".login-status").attr("data-status", "login");
+		$(".login-status").find(".menu-li__text").text("登入");
+	}
+}
+
+// 登入按鈕跳窗
+$(".login-status").on("click", function () {
+	// 登入 / 登出
+	if ($(".login-status").attr("data-status") == "login") {
+		$(".popup").removeClass("show");
+		$(".popup.login").addClass("show");
+	}
+	// 登入成功切換文字
+	toggleLoginText();
+});
+
+// 註冊按鈕跳窗
+$(".login-btn__reg").on("click", function () {
+	$(".popup").removeClass("show");
+	$(".popup.reg").addClass("show");
+	$(".popup-select").html("");
+	var obj = [
+		{ value: "a", text: "A" },
+		{ value: "b", text: "B" },
+		{ value: "c", text: "C" }
+	];
+	// 塞入申請單位
+	insertOption(obj);
+});
+
+// 忘記密碼按鈕跳窗
+$(".login-btn__forget").on("click", function () {
+	$(".popup").removeClass("show");
+	$(".popup.forget").addClass("show");
+});
+
+// 塞入申請單位
+function insertOption(obj) {
+	var options = `<option class="popup-option" value="-1">輸入申請單位</option>`;
+	$(".popup-select").html("");
+	for (var i = 0; i < obj.length; i++) {
+		options += `<option class="popup-option" value="${obj[i].value}">${obj[i].text}</option>`;
+	}
+	$(".popup-select").html(options);
+}
